aeb3739f39abcb96ba53707f7ba89263fa924934
Tatsuya Sakamoto <pgm.gonta@gmail.com>
2018-02-25 04:59:10 +0900

    modify

--- actions/deploy.sh
+++ actions/deploy.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/sh -ex
 
-readonly local PACKNAME=$1
-readonly local FILE_FORMAT=$2
+
+readonly local WORK_DIR=/tmp/
+readonly local PACKNAME=${ST2_ACTION_PACK_NAME}
+readonly local FILE_FORMAT=tar.gz
 readonly local FILE_NAME=$PACKNAME.$FILE_FORMAT
+readonly local GIT_REPO=https://github.com/pgmgonta/$PACKNAME.git
 
-git archive --format=$FILE_FORMAT https://github.com/pgmgonta/st2packs/$PACKNAME HEAD > $FILE_NAME
-
+cd $WORK_DIR
+git clone $GIT_REPO
+cd $WORK_DIR/$PACKNAME
+git archive --format=$FILE_FORMAT HEAD > $FILE_NAME
 gsutil cp $FILE_NAME  gs://stackstorm-deploy-test/packs/
+
+cd $WORK_DIR
+rm -rf $WORK_DIR/tmp/$PACKNAME
